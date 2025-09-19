09cf73ad8c695e745f4a915481f3d6603dba75cb
Benjamin468 <s2311767@u.tsukuba.ac.jp>
2025-09-19 14:02:48 +0900

    音声入力のUIを作成

---
+++ src/routes/voice-input.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import VoiceInputScreen from '../components/screens/VoiceInputScreen';
+
+const VoiceInputRoute: React.FC = () => {
+  return <VoiceInputScreen />;
+};
+
+export default VoiceInputRoute;
